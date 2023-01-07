0f03479482d36110ed606f5fa5fd2bff3a700ca7
klltx <kostya.svetashov.01@gmail.com>
2023-01-08 01:02:07 +0400

    add afterUpdate, appointment for doctor

--- src/api/doctor/content-types/doctor/lifecycles.js
+++ src/api/doctor/content-types/doctor/lifecycles.js
@@ -15,6 +15,20 @@ module.exports = {
 
     // do something to the result;
   },
+
+  async afterUpdate(event) {
+    const { result, params } = event;
+
+    const receptionDay = result.receptionDay;
+    if (receptionDay) {
+      await strapi.db.query("api::doctor.doctor").update({
+        where: { id: result.id },
+        data: {
+          reception: generateReception(receptionDay),
+        },
+      });
+    }
+  },
 };
 
 function getDateTime(date) {
@@ -26,20 +40,20 @@ function getDateTime(date) {
 function generateTimeRange(day, timeStart, timeEnd) {
   const start = new Date(`${day} ${timeStart}`);
   const end = new Date(`${day} ${timeEnd}`);
-  const res = [{ [getDateTime(start)]: true }];
+  const res = { [getDateTime(start)]: true };
 
   while (start < end) {
     start.setMinutes(start.getMinutes() + 30);
-    res.push({ [getDateTime(start)]: true });
+    res[getDateTime(start)] = true;
   }
 
   return res;
 }
 
 function generateReception(receptionDays) {
-  return receptionDays.map((v) => {
-    return {
-      [v.date]: generateTimeRange(v.date, v.timeStart, v.timeEnd),
-    };
+  const res = {};
+  receptionDays.forEach((v) => {
+    res[v.date] = generateTimeRange(v.date, v.timeStart, v.timeEnd);
   });
+  return res;
 }
--- src/api/doctor/controllers/doctor.js
+++ src/api/doctor/controllers/doctor.js
@@ -1,9 +1,30 @@
-'use strict';
+"use strict";
 
 /**
  * doctor controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::doctor.doctor');
+module.exports = createCoreController("api::doctor.doctor", ({ strapi }) => ({
+  async update(ctx) {
+    const { id } = ctx.params;
+    const req = ctx.request.body.reception.split(":");
+    const date = req[0];
+    const time = req[1] + ":" + req[2];
+
+    const entry = await strapi.entityService.findOne("api::doctor.doctor", id);
+
+    console.log(entry.reception);
+    console.log(date);
+    entry.reception[date][time] = false;
+
+    ctx.request.body.data = {
+      ...entry,
+    };
+
+    const response = await super.update(ctx);
+
+    return response;
+  },
+}));
