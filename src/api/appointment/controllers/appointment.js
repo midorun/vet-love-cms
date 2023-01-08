2ed0abb7d055e6d412ce2a3eef4ba09d8853d7fc
klltx <kostya.svetashov.01@gmail.com>
2023-01-08 23:11:55 +0300

    add appointment create controller

--- src/api/appointment/controllers/appointment.js
+++ src/api/appointment/controllers/appointment.js
@@ -1,52 +1,42 @@
-'use strict';
+"use strict";
 
 /**
  * appointment controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
+module.exports = createCoreController(
+  "api::appointment.appointment",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const { doctorId, time } = ctx.request.body.data;
 
-module.exports = createCoreController('api::appointment.appointment', ({ strapi }) => ({
-  // async create(ctx) {
-  //   const response = await super.create(ctx);
-  //   const { data: newAppointment } = response
-  //
-  //   const { body: { data } } = ctx.request
-  //
-  //   const { clientId, doctorId } = data
-  //
-  //   const client = await strapi.entityService.findOne("api::client.client", clientId, {
-  //     fields: [],
-  //     populate: ['appointments']
-  //   })
-  //
-  //   if (client) {
-  //     const { appointments } = client
-  //
-  //     await strapi.entityService.update("api::client.client", clientId, {
-  //       data: {
-  //         appointments: [...appointments, newAppointment]
-  //       }
-  //     })
-  //   }
-  //
-  //   const doctor = await strapi.entityService.findOne("api::doctor.doctor", doctorId, {
-  //     fields: [],
-  //     populate: ['appointments']
-  //   })
-  //
-  //   if (doctor) {
-  //     const { appointments } = doctor
-  //
-  //     await strapi.entityService.update("api::doctor.doctor", doctorId, {
-  //       data: {
-  //         appointments: [...appointments, newAppointment]
-  //       }
-  //     })
-  //   }
-  //
-  //   return response;
-  // }
-}));
+      ctx.request.body.data = {
+        ...ctx.request.body.data,
+        time: new Date(time),
+      };
 
+      const doctor = await strapi.entityService.findOne(
+        "api::doctor.doctor",
+        doctorId
+      );
+
+      if (doctor) {
+        const date = time.slice(0, 10);
+        const hours = time.slice(-5);
+        doctor.reception[date][hours] = false;
+
+        await strapi.entityService.update("api::doctor.doctor", doctorId, {
+          data: {
+            reception: doctor.reception,
+          },
+        });
+      }
+
+      const response = await super.create(ctx);
+
+      return response;
+    },
+  })
+);
--- src/api/doctor/controllers/doctor.js
+++ src/api/doctor/controllers/doctor.js
@@ -6,25 +6,4 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::doctor.doctor", ({ strapi }) => ({
-  async update(ctx) {
-    const { id } = ctx.params;
-    const req = ctx.request.body.reception.split(":");
-    const date = req[0];
-    const time = req[1] + ":" + req[2];
-
-    const entry = await strapi.entityService.findOne("api::doctor.doctor", id);
-
-    console.log(entry.reception);
-    console.log(date);
-    entry.reception[date][time] = false;
-
-    ctx.request.body.data = {
-      ...entry,
-    };
-
-    const response = await super.update(ctx);
-
-    return response;
-  },
-}));
+module.exports = createCoreController("api::doctor.doctor");
